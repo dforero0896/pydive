1f3724e0a6cd327f0d935af6b4be32bd3b54c096
Daniel Felipe Forero Sanchez <df.forero10@gmail.com>
2020-10-27 14:28:42 +0100

    Created a function to get voids from galaxies so code is easier to use.

--- dive.py
+++ dive.py
@@ -6,43 +6,64 @@ import os
 import sys
 import argparse
 
-if __name__=='__main__':
+def galaxy_to_void_catalog(points, r_min, r_max, box_size, is_box=False, cpy_range=80):
 
-    data_fn = "tests/points.dat"
-    is_box=True
-    points = pd.read_csv(data_fn, delim_whitespace=True, engine='c', names=['x', 'y', 'z'], usecols=[0,1,2]).values
+    print(f"==> {points.shape[0]} tracers found.")
     if is_box:
-        box_size=2500
         low_range = 0
         high_range = box_size
-        cpy_range = 80
         out_range=0
+        print("==> Duplicating boundaries for periodic condition", flush=True)
         for i in range(3):
             lower = points[points[:,i] < low_range + cpy_range]
             lower[:,i] += box_size
-            points = np.append(points, lower, axis=0)
+            points = np.append(points, lower, axis=0) #This is not memory efficient.
             higher = points[points[:,i] >= high_range - cpy_range]
             higher[:,i] -= box_size
             points = np.append(points, higher, axis=0)
-        print(points.shape)
         del lower, higher
-    print(f"==> Number of tracers: {points.shape[0]}")
-    print(f"==> Building Delaunay Triangulation")
+    print(f"==> Number of vertices: {points.shape[0]}")
+    print(f"==> Building Delaunay Triangulation")   
     tess = Delaunay(points)
+    del points
     simplex_coords=tess.points[tess.simplices[:,:], :]
+    del tess
     n_simplices = simplex_coords.shape[0]
     print(f"==> Found {n_simplices} simplices", flush=True)
     result = np.empty((n_simplices, 4), dtype=np.double)
     print(f"==> Computing centers and radii")
     get_void_catalog(simplex_coords, result, n_simplices)
-    print(result)
+    del simplex_coords
+    #get_void_catalog_parallel(simplex_coords, result, n_simplices)
+    result=result.astype(np.float32)
     if is_box:
         for i in range(3):
             mask = (result[:,i] > low_range - out_range) & (result[:,i] < high_range + out_range)
             result = result[mask]
-    print(f"==> Saving")
+    # Radius mask
+    mask = (result[:,3] > r_min) & (result[:,3] < r_max)
+    
+    result = result[mask] 
+    return result
+
+if __name__=='__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-in", "--input_catalog", default = "tests/points.dat")
+    parser.add_argument("-out", "--output_catalog", default = "tests/voids_pydive.dat")
+    parser.add_argument("-box", "--box_size", dest="box_size", default=2500, type=np.float64)
+    parser.add_argument("-rmin", "--rmin", dest="rmin", default=0, type=np.float64)
+    parser.add_argument("-rmax", "--rmax", dest="rmax", default=999, type=np.float64)
+    args = parser.parse_args()
+    data_fn = args.input_catalog
+    is_box=True
+    box_size=2500
+    print(f"==> Reading file {data_fn}", flush=True)
+    points = pd.read_csv(data_fn, delim_whitespace=True, engine='c', names=['x', 'y', 'z'], usecols=[0,1,2]).values.astype(np.float64)
+    result = galaxy_to_void_catalog(points, args.rmin, args.rmax, box_size, is_box, cpy_range=80)
+    print(f"==> Saving voids with radius ({args.rmin}, {args.rmax})")
     #c_ascii_writer(result, n_simplices, 'tests/voids_pydive.dat')
-    np.savetxt('tests/voids_pydive.dat', result, fmt="%.8f")
-    #np.save('tests/voids_pydive.npy', result)
+    #np.savetxt(args.output_catalog, result, fmt="%.8f")
+    np.save('tests/voids_pydive.npy', result)
     print("==> Finished successfully")
 
